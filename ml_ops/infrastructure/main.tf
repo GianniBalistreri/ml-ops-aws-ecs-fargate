190650c5205cdcd1b731c88cf314f610cae6bcfe
giannibalistreri <gbalistreri762@gmail.com>
2022-04-19 00:21:48 +0200

    Add s3 backend

--- ml_ops/infrastructure/main.tf
+++ ml_ops/infrastructure/main.tf
@@ -1,12 +1,8 @@
-## Terraform config:
-#terraform {
-#  backend "s3" {
-#    bucket = var.s3_bucket_name_infrastructure
-#    key = var.s3_bucket_name_infrastructure_tf_state
-#    region = var.region
-#  }
-#}
-
-#terraform {
-#  required_version = ">= 1.0.0"
-#}
+# Terraform config:
+terraform {
+  backend "s3" {
+    bucket = "gfb-ml-ops-tf-infrastructure"
+    key = "ecs-fargate/tf-state"
+    region = "eu-central-1"
+  }
+}
