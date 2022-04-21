110172253057c82630f2b8dca4d85dd84aa1c869
giannibalistreri <gbalistreri762@gmail.com>
2022-04-21 14:14:17 +0200

    Refactoring

--- ml_ops/infrastructure/ecs.tf
+++ ml_ops/infrastructure/ecs.tf
@@ -92,15 +92,3 @@ resource "aws_ecs_service" "main" {
     aws_iam_role_policy_attachment.ecs_task_execution_role
   ]
 }
-
-# ECS task execution role
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = var.ecs_task_execution_role_name
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
-}
-
-# ECS task execution role policy attachment
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
--- ml_ops/infrastructure/iam_roles.tf
+++ ml_ops/infrastructure/iam_roles.tf
@@ -70,6 +70,18 @@ data "aws_iam_policy_document" "s3" {
 # AWS ECS IAM Role: #
 #####################
 
+# ECS task execution role
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = var.ecs_task_execution_role_name
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
+}
+
+# ECS task execution role policy attachment
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 #resource "aws_iam_role" "ecs_task_execution_role" {
 #  name = var.ecs_task_execution_role_name
 #  assume_role_policy = jsonencode({
